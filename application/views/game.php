d15d911459d314eea8314377c79976f1f8f46a8c
Rin <Rin@Rin-HP>
2016-02-14 17:53:17 -0800

    added game page, as well as functions on page, hopefully working

---
+++ application/views/game.php
@@ -0,0 +1,15 @@
+<div id="outer">
+    <div id="left">
+        <div>
+        {currparts}
+         <div/>
+         <div>
+         {knownpieces}
+        <div/>
+    <div/>
+
+    <div id="right">
+        {equitylist}
+    <div/>
+<div/>
+
